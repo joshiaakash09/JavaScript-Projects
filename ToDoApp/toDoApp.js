68a794dbe1208a4798efda611da0db2c6d00ae5f
pheonix <aakash_joshi@outlook.com>
2021-02-09 13:20:53 -0600

    updating JS file

---
+++ ToDoApp/toDoApp.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('task');
+const taskList = document.querySelector('collection');
+const filter = document.querySelector('#filter');
+const clearBtn = document.querySelector('.clear-tasks');

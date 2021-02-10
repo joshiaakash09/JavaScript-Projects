0230f04b2e5b98771c4bce6e7862439ba50040c1
pheonix <aakash_joshi@outlook.com>
2021-02-09 18:34:17 -0600

    good

--- ToDoApp/toDoApp.js
+++ ToDoApp/toDoApp.js
@@ -1,5 +1,34 @@
+// Define UI Variables
+
 const form = document.querySelector('#task-form');
 const taskInput = document.querySelector('task');
 const taskList = document.querySelector('collection');
 const filter = document.querySelector('#filter');
 const clearBtn = document.querySelector('.clear-tasks');
+
+// load all event listners
+
+function loadEventListners(){
+    // DOM Load event
+    document.addEventListener('DOMContentLoaded', getTasks);
+    // Add task event
+    form.addEventListener('click', addTask);
+    // Remove task event
+    taskList.querySelector('click', removeTask);
+    //Clear task event
+    clearBtn.querySelector('click', clearTasks);
+    // Filter tassk events
+    filter.querySelector('click', filterTasks);
+}
+
+//Get tasks from LS
+
+function getTasks (){
+   let tasks;
+
+   if(localStorage.getItem('tasks') === null){
+       tasks =[];
+   }else{
+
+   }
+}

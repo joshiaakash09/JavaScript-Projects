7183a493ac106ecab268a5bf4b9560719bb86566
pheonix <aakash_joshi@outlook.com>
2021-02-14 00:55:32 -0600

    Validation and winning case

---
+++ Number Prediction Game/README.md
@@ -0,0 +1,9 @@
+This is Number Prediction app. A fun way to predict number from a random number generating app.
+
+
+GAME FUNCTION:
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let player choose to play again
---
+++ Number Prediction Game/app.js
@@ -0,0 +1,46 @@
+// Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+      
+// Listen for guess
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
+  
+  // Validate
+  if(isNaN(guess) || guess < min || guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
+
+  // Check if won
+  if(guess === winningNum){
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = 'green';
+    // Set message
+    setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+
+  } else {
+
+  }
+});
+
+// Set message
+function setMessage(msg, color){
+  message.style.color = color;
+  message.textContent = msg;
+}

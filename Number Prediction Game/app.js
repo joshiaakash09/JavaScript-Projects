4faac1f8bc4fb53c670088b28525daa229afe5fe
pheonix <aakash_joshi@outlook.com>
2021-02-14 11:29:21 -0600

    Lose case and game over

--- Number Prediction Game/app.js
+++ Number Prediction Game/app.js
@@ -27,17 +27,45 @@ guessBtn.addEventListener('click', function(){
 
   // Check if won
   if(guess === winningNum){
-    // Disable input
-    guessInput.disabled = true;
-    // Change border color
-    guessInput.style.borderColor = 'green';
-    // Set message
-    setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+    // Game over - won
+    gameOver(true, `${winningNum} is correct, YOU WIN!`);
 
   } else {
+    // Wrong number
+    guessesLeft -= 1;
 
+    if(guessesLeft === 0){
+      // Game over - lost
+      gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
+    } else {
+      // Game continues - answer wrong
+
+      // Change border color
+      guessInput.style.borderColor = 'red';
+
+      // Clear Input
+      guessInput.value = '';
+
+      // Tell user its the wrong number
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+    }
   }
 });
+
+// Game over
+function gameOver(won, msg){
+  let color;
+  won === true ? color = 'green' : color = 'red';
+
+  // Disable input
+  guessInput.disabled = true;
+  // Change border color
+  guessInput.style.borderColor = color;
+  // Set text color
+  message.style.color = color;
+  // Set message
+  setMessage(msg);
+}
 
 // Set message
 function setMessage(msg, color){

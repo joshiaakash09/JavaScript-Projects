6f185682162b5f2f1cf906288f4ace890cf77495
pheonix <aakash_joshi@outlook.com>
2021-02-15 08:40:16 -0600

    final update

--- Number Prediction Game/app.js
+++ Number Prediction Game/app.js
@@ -1,7 +1,7 @@
 // Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 // UI Elements
@@ -15,6 +15,13 @@ const game = document.querySelector('#game'),
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Play again event listener
+game.addEventListener('mousedown', function(e){
+  if(e.target.className === 'play-again'){
+    window.location.reload();
+  }
+});
       
 // Listen for guess
 guessBtn.addEventListener('click', function(){
@@ -65,6 +72,15 @@ function gameOver(won, msg){
   message.style.color = color;
   // Set message
   setMessage(msg);
+
+  // PLay Again?
+  guessBtn.value = 'Play Again';
+  guessBtn.className += 'play-again';
+}
+
+// Get Winning Number
+function getRandomNum(min, max){
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 // Set message
